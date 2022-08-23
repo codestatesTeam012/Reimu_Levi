fa195b04a9320548b734992fcc44ccbcaa777776
JEONGHWANMIN <mimi0j0j@naver.com>
2022-08-24 02:06:49 +0900

    ✨ feat : Redux-toolkit store 파일 추가

---
+++ src/redux/store/index.ts
@@ -0,0 +1,24 @@
+import {configureStore} from '@reduxjs/toolkit'
+import {TypedUseSelectorHook, useDispatch, useSelector} from 'react-redux'
+import testSlice from '../reducers/testSlice'
+
+export const store = configureStore({
+  reducer: {
+    // 사용할 Slice를 reducer에 등록 해주세요 !
+    test: testSlice,
+  },
+})
+
+export type RootState = ReturnType<typeof store.getState>
+export type AppDispatch = typeof store.dispatch
+
+/*
+사용할 때 useAppDispatch(), useAppSelector() 를 사용해 주세요 !
+Ex) Page 사용 예시
+const dispatch = useAppDispatch();
+const reservations = useAppSelector((state: RootState) => state.reservations.value);
+자세한건 pages안에 Example파일을 참고해주세요.
+*/
+
+export const useAppDispatch: () => AppDispatch = useDispatch
+export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector
