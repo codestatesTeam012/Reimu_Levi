fe64b252695f14f450278233d5e9699699547580
JEONGHWANMIN <mimi0j0j@naver.com>
2022-08-24 02:02:48 +0900

    ✨ feat : 리덕스 툴킷 테스팅 라우팅 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import {BrowserRouter, Routes, Route} from 'react-router-dom'
-import Home from './pages/Home'
+import {Home, Example} from './pages'
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/test" element={<Example />} />
       </Routes>
     </BrowserRouter>
   )
