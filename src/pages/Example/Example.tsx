6153e8307d092f1221785328b99fa192457b4b32
JEONGHWANMIN <mimi0j0j@naver.com>
2022-08-24 10:43:57 +0900

    ✨ feat : redux-thunk 비동기 예제 추가

--- src/pages/Example/Example.tsx
+++ src/pages/Example/Example.tsx
@@ -1,16 +1,25 @@
-import React, {useState} from 'react'
+import React, {useEffect, useState} from 'react'
 import {addCount} from 'src/redux/reducers/testSlice'
-import {RootState, useAppDispatch, useAppSelector} from 'src/redux/store'
+import {RootState, store, useAppDispatch, useAppSelector} from 'src/redux/store'
+import {getAllTodo} from 'src/redux/thunkActions/testAction'
 
 const Example = () => {
   const [num] = useState(2)
   // useAppSelector() , useAppDispatch() 사용 예제
-  const count = useAppSelector((state: RootState) => state.test.count)
+  // const count = useAppSelector((state: RootState) => state.test.count)
+  const {count, isLoading, error} = useAppSelector((state: RootState) => state.test)
   const dispatch = useAppDispatch()
+  console.log(count)
+
+  useEffect(() => {
+    store.dispatch(getAllTodo(null))
+  }, [])
 
   const handleUp = () => {
     dispatch(addCount(num))
   }
+  if (error) return <span> Error!! 👻</span>
+  if (isLoading) return <span> isLoading 👻</span>
   return (
     <>
       <h1>{count}</h1>
