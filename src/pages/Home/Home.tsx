3e6612cecb7cc46ff37a3f01135f877ddd9a7a78
JEONGHWANMIN <mimi0j0j@naver.com>
2022-08-25 03:02:55 +0900

    ✨ feat: 홈 컴포넌트 추가

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,15 +1,7 @@
-import axios from 'axios'
-import React, {useEffect} from 'react'
+import React from 'react'
 
 const Home = () => {
-  useEffect(() => {
-    axios.get('http://3.35.208.55:8080/v1/example').then((res) => console.log(res))
-  }, [])
-  return (
-    <>
-      <h1>Hello Home</h1>
-    </>
-  )
+  return <div>Home</div>
 }
 
 export default Home
