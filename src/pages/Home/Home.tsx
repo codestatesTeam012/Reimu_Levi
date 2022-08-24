f4395e288eaa1ae9fe4bc954465502c264cc25bd
JEONGHWANMIN <mimi0j0j@naver.com>
2022-08-24 10:46:05 +0900

    ✨ feat : api 테스트 추가

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,6 +1,10 @@
-import React from 'react'
+import axios from 'axios'
+import React, {useEffect} from 'react'
 
 const Home = () => {
+  useEffect(() => {
+    axios.get('http://3.35.208.55:8080/v1/example').then((res) => console.log(res))
+  }, [])
   return (
     <>
       <h1>Hello Home</h1>
