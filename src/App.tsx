cf89ffb1d3d08c46f51752e0dff32e636815d4fd
JEONGHWANMIN <mimi0j0j@naver.com>
2022-08-25 03:02:27 +0900

    ✨ feat: Header 컴포넌트 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import {BrowserRouter, Routes, Route} from 'react-router-dom'
 import Footer from './components/Footer'
+import Header from './components/Header/Header'
 import {Home, Example} from './pages'
 
 function App() {
   return (
     <BrowserRouter>
+      <Header />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/test" element={<Example />} />
