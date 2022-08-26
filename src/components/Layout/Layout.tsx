dfd1c69b65406caf2b3ed5f11f8d556a725bc5b3
JEONGHWANMIN <mimi0j0j@naver.com>
2022-08-26 22:09:14 +0900

    💄style: 반응형 css 속성 추가

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import styled from 'styled-components'
+import Footer from '../Footer'
 import LeftSideBar from '../LeftSideBar/LeftSideBar'
 
 interface Props {
@@ -18,13 +19,16 @@ const Layout = ({children}: Props) => {
         </LeftNav>
         <RightContent>{children}</RightContent>
       </MainBox>
+      <Footer />
     </Container>
   )
 }
 
 export default Layout
 
-const Container = styled.div``
+const Container = styled.div`
+  height: 100%;
+`
 
 const Blank = styled.div`
   padding-top: 5rem;
@@ -35,16 +39,22 @@ const MainBox = styled.div`
   max-width: 1300px;
   margin: 0 auto;
   width: 100%;
+  height: 100%;
 `
 
 const LeftNav = styled.div`
   position: relative;
   width: 16%;
+
+  @media (max-width: 900px) {
+    display: none;
+  }
 `
 
 const FixBox = styled.div`
   /* block / fixed? */
-  position: block;
+  position: fixed;
+  /* position: block; */
 `
 
 const RightContent = styled.div`
