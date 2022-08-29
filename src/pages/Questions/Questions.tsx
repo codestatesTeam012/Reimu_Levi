80245523c5fbb8853123b0dae0a64b2d8e5bf74a
JEONGHWANMIN <mimi0j0j@naver.com>
2022-08-29 22:35:30 +0900

    ✨feat	: 오른쪽 사이드바 레이아웃 마무리

--- src/pages/Questions/Questions.tsx
+++ src/pages/Questions/Questions.tsx
@@ -1,10 +1,25 @@
+/* eslint-disable react/jsx-no-undef */
 import React from 'react'
+import Pen from 'src/assets/svgComponents/Pen'
 // import {useNavigate} from 'react-router-dom'
 import AuthButton from 'src/components/AuthButton/AuthButton'
 import Layout from 'src/components/Layout/Layout'
+import PosticCard from 'src/components/PosticCard/PosticCard'
 import QuestionCard from 'src/components/QuestionCard.tsx/QuestionCard'
 import {toLocalScale} from 'src/utils/utill'
 import styled from 'styled-components'
+
+const contents1 = [
+  {icon: Pen, content: 'Open source and accidental innovation'},
+  {icon: Pen, content: 'The luckiest guy in AI (Ep. 477)'},
+]
+const contents2 = [
+  {icon: Pen, content: 'Recent site instability, major outages – July/August 2022'},
+  {icon: Pen, content: 'Please welcome Valued Associate #1301 - Emerson'},
+  {icon: Pen, content: 'Staging Ground Workflow: Question Lifecycle'},
+  {icon: Pen, content: 'Collectives Update: WSO2 launches, and Google Go sunsets'},
+  {icon: Pen, content: 'Should we burninate the [option] tag?'},
+]
 
 const Questions = () => {
   // const navigate = useNavigate()
@@ -39,7 +54,12 @@ const Questions = () => {
           </QuestionsContent>
           <Paginations>페이지 네이션 넣을 곳</Paginations>
         </LeftBox>
-        <RightBox>2</RightBox>
+        <RightBox>
+          <Postic>
+            <PosticCard title="The Overflow Blog" contents={contents1} />
+            <PosticCard title="Featured on Meta" contents={contents2} />
+          </Postic>
+        </RightBox>
       </QuestionsBox>
     </Layout>
   )
@@ -67,7 +87,7 @@ const LeftBox = styled.div`
 const RightBox = styled.div`
   margin-left: 1rem;
   width: 32rem;
-  background-color: skyblue;
+  /* background-color: skyblue; */
 `
 const QuestionsHeader = styled.div`
   height: 6rem;
@@ -79,7 +99,6 @@ const QuestionsHeader = styled.div`
   h1 {
     font-weight: 500;
     font-size: 2.5rem;
-    font-family: 'Rubik Iso', cursive;
   }
 `
 const QuestionSubHeader = styled.div`
@@ -99,3 +118,11 @@ const QuestionsContent = styled.div`
 `
 
 const Paginations = styled.div``
+
+const Postic = styled.div`
+  margin-top: 2rem;
+  box-shadow: 0 1px 2px hsla(0, 0%, 0%, 0.05), 0 1px 4px hsla(0, 0%, 0%, 0.05),
+    0 2px 8px hsla(0, 0%, 0%, 0.05);
+  border-radius: 3px;
+  border: solid 1px hsl(210, 8%, 85%);
+`
