ade386c65e3c82937dd30d1425f07cab672ab09d
JEONGHWANMIN <mimi0j0j@naver.com>
2022-08-30 23:13:08 +0900

    ✨feat : 로그인 페이지 레이아웃

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -1,7 +1,129 @@
-import React from 'react'
+import axios from 'axios'
+import React, {useEffect} from 'react'
+import Logo from 'src/assets/svgComponents/Logo'
+import AuthButton from 'src/components/AuthButton/AuthButton'
+import SocialButton from 'src/components/SocialButton/SocialButton'
+import styled from 'styled-components'
 
 const Login = () => {
-  return <div>Login</div>
+  console.log('sds')
+  useEffect(() => {
+    // axios
+    //   .get('http://3.35.20.134:8080/v1/posts/1')
+    //   .then((res) => console.log(res))
+    //   .catch((err) => console.log(err.message))
+  }, [])
+
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+    const formdata = new FormData(e.currentTarget)
+    const username = formdata.get('username')
+    const password = formdata.get('password')
+    console.log(username, password)
+    const form = {
+      username,
+      password,
+    }
+    const result = await axios.post('http://3.35.20.134:8080/login', form)
+    console.log(result)
+  }
+  return (
+    <Container>
+      <Blank></Blank>
+      <MainContent>
+        <LogoBox>
+          <Logo />
+        </LogoBox>
+        <SocialButton />
+        <LoginBox>
+          <LoginForm onSubmit={handleSubmit}>
+            <label>Eamil</label>
+            <input type="text" name="username" />
+            <LabelBox>
+              <label>Password</label>
+              <span>Forgot password?</span>
+            </LabelBox>
+            <input type="password" name="password" />
+            <ButtonBox>
+              <AuthButton mode="SignUp" width={23} text="Log in" />
+            </ButtonBox>
+          </LoginForm>
+        </LoginBox>
+        <TextBox>asdas</TextBox>
+      </MainContent>
+    </Container>
+  )
 }
 
 export default Login
+
+const Container = styled.div`
+  background-color: hsl(210, 8%, 95%);
+  height: 100vh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`
+
+const Blank = styled.div`
+  height: 5rem;
+`
+
+const MainContent = styled.div`
+  width: 29rem;
+  height: 57.2rem;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`
+
+const LogoBox = styled.div`
+  margin-bottom: 2rem;
+`
+
+const LoginBox = styled.div`
+  margin-top: 2rem;
+  background-color: white;
+  width: 100%;
+  height: 23.6rem;
+  border-radius: 1rem;
+`
+
+const LoginForm = styled.form`
+  display: flex;
+  flex-direction: column;
+  width: 80%;
+  margin: 0 auto;
+
+  label {
+    margin-top: 2rem;
+    color: hsl(210, 8%, 5%);
+    font-size: 1.5rem;
+    font-weight: bold;
+  }
+
+  span {
+    font-size: 1.1rem;
+    margin-top: 2rem;
+    color: hsl(206, 100%, 40%);
+    cursor: pointer;
+  }
+  input {
+    margin-top: 1rem;
+    height: 3.7rem;
+    border: solid 1px lightgray;
+    border-radius: 3px;
+  }
+`
+
+const LabelBox = styled.div`
+  display: flex;
+  justify-content: space-between;
+`
+
+const ButtonBox = styled.div`
+  margin-right: 2rem;
+  margin-top: 2rem;
+`
+
+const TextBox = styled.div``
