0bc3d0ac1e1933911c37d4c8821f9c224130b434
JEONGHWANMIN <mimi0j0j@naver.com>
2022-09-01 23:49:59 +0900

    ✨feat : 페이지 인포 정보 저장

--- src/redux/thunkActions/postsAction.ts
+++ src/redux/thunkActions/postsAction.ts
@@ -1,11 +1,16 @@
 import {createAsyncThunk} from '@reduxjs/toolkit'
 import {viewPostService} from 'src/apis/ViewPostAPI'
-import {Posts} from '../reducers/postsSlice'
+import {PageInfo, Post} from '../reducers/postsSlice'
 import {CreateAsyncThunkTypes} from '../store'
 
 // createAsyncThunk<리턴타입 , 받는인자타입, ThunkAPI 사용할때 타입>
 
-export const getPostsThunk = createAsyncThunk<Posts[], number, CreateAsyncThunkTypes>(
+interface ReturnType {
+  data: Post[]
+  pageInfo: PageInfo
+}
+
+export const getPostsThunk = createAsyncThunk<ReturnType, number, CreateAsyncThunkTypes>(
   'posts/getPosts',
   async (pageNum) => {
     const result = await viewPostService.getPosts(pageNum)
