422cc738e2552059c1fe29b7b09d5b11cfca8b7a
JEONGHWANMIN <mimi0j0j@naver.com>
2022-09-02 16:54:32 +0900

    ✨feat : 글 수정 페이지 라우팅추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import {BrowserRouter, Routes, Route} from 'react-router-dom'
 import Header from './components/Header/Header'
-import {Home, Example, Login, Questions, Tags, Users, SignUp, Detail, Write} from './pages'
+import {Home, Example, Login, Questions, Tags, Users, SignUp, Detail, Write, Edit} from './pages'
 
 function App() {
   return (
@@ -12,6 +12,7 @@ function App() {
         <Route path="/questions" element={<Questions />} />
         <Route path="/questions/:id" element={<Detail />} />
         <Route path="/write" element={<Write />} />
+        <Route path="/edit/:id" element={<Edit />} />
         <Route path="/tags" element={<Tags />} />
         <Route path="/users" element={<Users />} />
         <Route path="/login" element={<Login />} />
