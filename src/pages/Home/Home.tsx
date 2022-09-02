f037fc45b7790d69c2ff6a77d63c6dcf7f74ff2a
Sujeong Ji <suji.sujeongji@gmail.com>
2022-09-02 16:57:45 +0900

    🔨refactor : Home 페이지 레이아웃 및 CSS 리팩토링

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,51 +1,107 @@
 import React from 'react'
-import {useNavigate} from 'react-router-dom'
 import styled from 'styled-components'
 import TextBalloonLeft from '../../components/TextBalloon/TextBalloonLeft'
 import TextBalloonRight from 'src/components/TextBalloon/TextBalloonRight'
+import Footer from 'src/components/Footer'
+
+type Record = {
+  recordNumber: string
+  recordText: string
+}
 
 const Home = () => {
-  const navigate = useNavigate()
+  const recordsInfo: Record[] = [
+    {
+      recordNumber: '100+ million',
+      recordText: 'monthly visitors to Stack Overflow &amp; Stack Exchange',
+    },
+    {
+      recordNumber: '45.1 billion',
+      recordText: 'Times a developer got help since 2008',
+    },
+    {
+      recordNumber: '179% ROI',
+      recordText: 'from companies using Stack Overflow for Teams',
+    },
+    {
+      recordNumber: '5,000+',
+      recordText: 'Stack Overflow for Teams instances active every day',
+    },
+  ]
+
   return (
-    <Main>
-      <Wrapper>
-        <ContainerDeco aria-hidden="true"></ContainerDeco>
-        <ContainerDeco aria-hidden="true"></ContainerDeco>
-        <ContainerDeco aria-hidden="true"></ContainerDeco>
-        <ContainerDeco aria-hidden="true"></ContainerDeco>
-        <ContainerDeco aria-hidden="true"></ContainerDeco>
-        <ContainerDeco aria-hidden="true"></ContainerDeco>
-        <Container>
-          <SectionOne>
-            <SectionOneDeco>
-              <SectionOneImg
-                src="https://cdn.sstatic.net/Img/home/illo-code.svg?v=b7ee00fff9d8"
-                alt="bg-deco"
-              />
-              <SectionOneImg
-                src="https://cdn.sstatic.net/Img/home/illo-code.svg?v=b7ee00fff9d8"
-                alt=""
-              />
-            </SectionOneDeco>
-            <ContentOne>
-              <TextBalloonLeft />
-              <TextBalloonRight />
-            </ContentOne>
-            <div className="content-three"></div>
-            <div className="content-four"></div>
-          </SectionOne>
-          <SectionTwo>
-            <div className="content-five"></div>
-          </SectionTwo>
-        </Container>
-      </Wrapper>
-    </Main>
+    <Section>
+      <Main>
+        <Wrapper>
+          <ContainerDeco aria-hidden="true" />
+          <ContainerDeco aria-hidden="true" />
+          <ContainerDeco aria-hidden="true" />
+          <ContainerDeco aria-hidden="true" />
+          <ContainerDeco aria-hidden="true" />
+          <ContainerDeco aria-hidden="true" />
+          <ContainerDeco aria-hidden="true" />
+          <ContainerDeco aria-hidden="true" />
+          <Container>
+            <SectionOne>
+              <SectionOneDeco>
+                <SectionOneImg
+                  src="https://cdn.sstatic.net/Img/home/illo-code.svg?v=b7ee00fff9d8"
+                  alt="bg-deco"
+                />
+                <SectionOneImg
+                  src="https://cdn.sstatic.net/Img/home/illo-code.svg?v=b7ee00fff9d8"
+                  alt=""
+                />
+              </SectionOneDeco>
+              <ContentOne>
+                <ContentOneWrapper>
+                  <TextBalloonLeft />
+                </ContentOneWrapper>
+                <ContentOneWrapper>
+                  <TextBalloonRight />
+                </ContentOneWrapper>
+                <ContentOneDeco aria-hidden="true" />
+                <ContentOneDeco aria-hidden="true" />
+              </ContentOne>
+              <ContentTwo>
+                <WordsSlider>
+                  <RollingWords>Every </RollingWords>
+                  <RollingWords color="rgb(242, 116, 14)">Frontend Developer </RollingWords>
+                  <RollingWords>has a tab open to </RollingWords>
+                  <RollingWords>Stack Overflow</RollingWords>
+                </WordsSlider>
+              </ContentTwo>
+              <ContentThree>
+                <LineBreak />
+              </ContentThree>
+              <ContentFour>
+                {recordsInfo.map((info, idx) => (
+                  <Records key={idx}>
+                    <RecordNumber>{info.recordNumber}</RecordNumber>
+                    <RecordText>{info.recordText}</RecordText>
+                  </Records>
+                ))}
+              </ContentFour>
+            </SectionOne>
+          </Container>
+        </Wrapper>
+      </Main>
+      <Footer />
+    </Section>
   )
 }
 
 export default Home
 
+const Section = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`
+
 const Main = styled.div`
+  max-width: 194rem;
   margin-top: 5rem;
   min-height: 100vh;
   width: 100%;
@@ -53,14 +109,10 @@ const Main = styled.div`
   margin-top: 5rem;
   position: relative;
   z-index: 1;
-  display: flex;
-  justify-content: center;
-  align-items: center;
 `
 
 const Wrapper = styled.div`
-  flex: 1;
-  max-width: 194rem;
+  height: 100%;
   padding: 5rem;
   position: relative;
   z-index: 2;
@@ -90,6 +142,76 @@ const ContainerDeco = styled.span`
     right: 18rem;
     position: absolute;
   }
+
+  :nth-child(3) {
+    content: '';
+    width: 3.2rem;
+    height: 3.2rem;
+    background-color: rgb(71, 168, 104);
+    border-radius: 0.7rem 0.7rem 0.2rem;
+    top: 46rem;
+    left: -1.3rem;
+    position: absolute;
+  }
+
+  :nth-child(4) {
+    content: '';
+    width: 3.2rem;
+    height: 3.2rem;
+    background-color: rgb(214, 232, 220);
+    border-radius: 0.1rem 0.7rem 0.7rem;
+    top: 49.1rem;
+    left: 1.8rem;
+    position: absolute;
+  }
+
+  :nth-child(5) {
+    content: '';
+    width: 3.2rem;
+    height: 3.2rem;
+    background-color: rgb(255, 204, 1);
+    border-radius: 0.7rem 0.7rem 0.2rem;
+    top: 13rem;
+    left: 13rem;
+    position: absolute;
+    z-index: 99 !important;
+  }
+
+  :nth-child(6) {
+    content: '';
+    width: 3.2rem;
+    height: 3.2rem;
+    background-color: rgba(255, 204, 1, 0.238);
+    border-radius: 0.1rem 0.7rem 0.7rem;
+    top: 16.2rem;
+    left: 16.2rem;
+    position: absolute;
+    z-index: 99;
+  }
+
+  :nth-child(7) {
+    content: '';
+    width: 6.5rem;
+    height: 6.5rem;
+    background-color: rgb(242, 116, 14);
+    border-radius: 0.7rem 0.7rem 0.2rem;
+    top: 47rem;
+    right: 6.5rem;
+    position: absolute;
+    z-index: 99;
+  }
+
+  :nth-child(8) {
+    content: '';
+    width: 6.5rem;
+    height: 6.5rem;
+    background-color: rgba(242, 117, 14, 0.221);
+    border-radius: 0.1rem 0.7rem 0.7rem;
+    top: 53.5rem;
+    right: 0rem;
+    position: absolute;
+    z-index: 99;
+  }
 `
 
 const Container = styled.div`
@@ -100,14 +222,15 @@ const Container = styled.div`
   margin-left: auto;
   margin-right: auto;
   position: relative;
-  clip-path: polygon(0% 0%, 100% 0, 100% 100%, 50% 90%, 0% 100%);
   z-index: 4;
+  padding-bottom: 12rem;
 `
 
 const SectionOne = styled.div`
   position: relative;
   flex: 50%;
 `
+
 const SectionOneDeco = styled.div`
   z-index: 5;
 `
@@ -120,7 +243,7 @@ const SectionOneImg = styled.img`
 
   :last-child {
     left: 8%;
-    top: 13rem;
+    top: 25rem;
   }
 `
 
@@ -130,13 +253,131 @@ const ContentOne = styled.div`
   justify-content: center;
   align-items: center;
   margin: 0 5rem;
-  /* margin-left: auto;
-  margin-right: auto; */
   font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell,
     'Open Sans', 'Helvetica Neue', sans-serif;
-  padding: 5rem;
+  padding: 5rem 2rem;
+
+  @media (max-width: 1100px) {
+    display: flex;
+    flex-direction: column;
+    padding: 5rem 1rem;
+  }
 `
 
-const SectionTwo = styled.div`
-  background-color: blue;
+const ContentOneWrapper = styled.div`
+  padding: 1rem;
+
+  @media (max-width: 1100px) {
+    margin-bottom: 3rem;
+  }
+`
+
+const ContentOneDeco = styled.div`
+  position: relative;
+
+  :nth-child(3) {
+    content: '';
+    width: 1.6rem;
+    height: 1.6rem;
+    background-color: rgb(39, 52, 139);
+    border-radius: 0.25rem 0.25rem 0.1rem;
+    top: 1.5rem;
+    right: 9.5rem;
+    position: absolute;
+  }
+
+  :nth-child(4) {
+    content: '';
+    width: 1.6rem;
+    height: 1.6rem;
+    background-color: rgb(37, 41, 62);
+    border-radius: 0.1rem 0.25rem 0.25rem;
+    top: 3.1rem;
+    right: 7.9rem;
+    position: absolute;
+  }
+`
+
+const ContentTwo = styled.div`
+  max-width: 80rem;
+  text-align: center;
+  padding: 5rem 0;
+  margin: 0 auto;
+  position: relative;
+`
+
+const WordsSlider = styled.span`
+  justify-content: center;
+  font-weight: 700;
+  font-size: 5rem;
+  font-family: 'Comfortaa', cursive;
+  font-family: 'Roboto Slab', serif;
+  line-height: 1.3;
+  color: rgb(255, 255, 255);
+
+  @media (max-width: 1180px) {
+    display: flex;
+    flex-direction: column;
+  }
+`
+
+const RollingWords = styled.span`
+  color: ${(props) => props.color || 'rgb(255, 255, 255)'};
+`
+
+const ContentThree = styled.div`
+  padding-bottom: 4rem;
+`
+
+const LineBreak = styled.span`
+  height: 0.9rem !important;
+  width: 6.5em !important;
+  display: block !important;
+  margin-left: auto !important;
+  margin-right: auto !important;
+  border-radius: 0.5rem;
+  background-color: rgb(106, 115, 124);
+`
+
+const ContentFour = styled.div`
+  max-width: 100rem;
+  padding: 3rem;
+  margin: auto;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  @media (max-width: 1100px) {
+    display: flex;
+    flex-direction: column;
+  }
+`
+
+const Records = styled.div`
+  text-align: center;
+  font-family: 'Comfortaa', cursive;
+  font-family: 'Roboto Slab', serif;
+  padding: 0 1rem;
+  margin: 0 1rem;
+
+  @media (max-width: 1180px) {
+    margin: 2rem 0;
+  }
+`
+
+const RecordNumber = styled.div`
+  font-size: 2rem;
+  color: rgb(255, 255, 255);
+  font-weight: 700;
+  margin: 0.5rem 0;
+`
+
+const RecordText = styled.div`
+  font-size: 1.4rem;
+  color: rgb(142, 150, 155);
+  font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell,
+    'Open Sans', 'Helvetica Neue', sans-serif;
+  padding: 0.5rem;
+  line-height: 1.2;
+  margin-top: 1rem;
 `
