453eb996360cc9f6641c0cd65fd29867c666fe98
JEONGHWANMIN <mimi0j0j@naver.com>
2022-09-02 16:56:52 +0900

    ✨feat : http url 변경

--- src/apis/ViewPostAPI.ts
+++ src/apis/ViewPostAPI.ts
@@ -5,7 +5,7 @@ const postAPI = axios.create({baseURL: SERVER_URL, timeout: 1000})
 
 export const getPosts = async (pageNum: number) => {
   try {
-    const result = await postAPI.get(`v1/posts?size=15&page=${pageNum}`)
+    const result = await postAPI.get(`/v1/posts?size=15&page=${pageNum}`)
     return result
   } catch {
     throw new Error('전체 질문 조회 실패 👻')
@@ -14,7 +14,7 @@ export const getPosts = async (pageNum: number) => {
 
 export const getPostById = async (id: number) => {
   try {
-    const result = await postAPI.get(`v1/posts/${id}`)
+    const result = await postAPI.get(`/v1/posts/${id}`)
     return result
   } catch {
     throw new Error('글 상세 조회 실패 👻')
